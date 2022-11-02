2e6138668d1d20ffed1a92e2de3a973157454d5a
Marcin Lech <arcinech@gmail.com>
2022-11-02 06:49:28 +0100

    Add: home page placeholder

---
+++ client/src/components/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import styles from './Home.module.scss';
+
+const Home = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
