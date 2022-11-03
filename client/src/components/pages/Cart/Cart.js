daffc0d63909d2c1924746d38b8cc2c5f582b173
Marcin Lech <arcinech@gmail.com>
2022-11-03 23:10:49 +0100

    WIP cart module

--- client/src/components/pages/Cart/Cart.js
+++ client/src/components/pages/Cart/Cart.js
@@ -2,16 +2,29 @@ import { allItems } from '../../../redux/cartSlice';
 import { useSelector } from 'react-redux';
 import CartItem from '../../common/CartItem/CartItem';
 import styles from './Cart.module.scss';
-
+import { useNavigate } from 'react-router-dom';
 const Cart = () => {
   const cartItems = useSelector(allItems);
+  const navigate = useNavigate();
+
+  const processToOrder = (e) => {
+    e.preventDefault();
+    navigate('/order');
+  };
 
   return (
     <section className={styles.root}>
       {!cartItems ? (
         <h2>Your cart is empty</h2>
       ) : (
-        cartItems.map((item) => <CartItem key={item.id} {...item} />)
+        <>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <a className={styles.button} onClick={processToOrder} href="/order">
+            Process order
+          </a>
+        </>
       )}
     </section>
   );
