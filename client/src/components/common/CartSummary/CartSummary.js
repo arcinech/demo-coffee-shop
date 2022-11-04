3a70aa2b63fcf373ce1492687d6c8a9c3a7aeec1
Marcin Lech <arcinech@gmail.com>
2022-11-04 13:59:58 +0100

    WIP cart summary

--- client/src/components/common/CartSummary/CartSummary.js
+++ client/src/components/common/CartSummary/CartSummary.js
@@ -1,5 +1,26 @@
 const CartSummary = ({ id, name }) => {
-  return;
+    const cartItems = useSelector((state) => state.cart.cartItems);
+
+    return (
+        <div className={styles.root}>
+            <h2>Cart Summary</h2>
+            {cartItems.map((item) => (
+                    <div className={styles.image}>
+                    <img src={image} alt={name} />
+                  </div>
+                  <div className={styles.details}>
+                    <h3>{name}</h3>
+                    <p>Quantity: {quantity}</p>
+                    <p>Price: {price}</p>
+                  </div>
+                  <div className={styles.notes}>
+                    <label htmlFor="notes">Notes</label>
+                    <textarea
+                      name="notes"
+                      onChange={(e) => setNotes(e.target.value)}
+                      value={notes}
+                    />
+                  </div>}
 };
 
 export default CartSummary;
