33b0a901e6218a0e24186d520c68637b28a173a3
Marcin Lech <arcinech@gmail.com>
2022-11-04 14:00:14 +0100

    Add: routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,19 @@ import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/views/Home/Home';
+import SingleProduct from './components/views/SingleProduct/SingleProduct';
+import Cart from './components/views/Cart/Cart';
+import OrderPage from './components/pages/OrderPage/OrderPage';
+
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orderpage" element={<OrderPage />} />
       </Routes>
       <Footer />
     </>
