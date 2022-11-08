7de14b76070c406e485517afa0e5fe51fd9a4038
Marcin Lech <arcinech@gmail.com>
2022-11-08 23:40:43 +0100

    Fix: home page

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,13 +1,28 @@
+import ProductGrid from '../../common/ProductGrid/ProductGrid';
+import Splash from '../../common/Splash/Splash';
 import styles from './Home.module.scss';
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../../../redux/productSlice';
 const Home = () => {
-  return (
-    <div className={styles.root}>
-      <div className={styles.container}>
-        <h1>Home</h1>
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.product.status);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  } else
+    return (
+      <div className={styles.root}>
+        <Splash />
+        <section className={styles.container}>
+          <ProductGrid />
+        </section>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
