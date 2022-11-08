fb7cfd18cca3a5cf3c26b812f3808fef4bc5003c
Marcin Lech <arcinech@gmail.com>
2022-11-08 23:35:26 +0100

    Fix: cart Summary

--- client/src/components/common/CartSummary/CartSummary.js
+++ client/src/components/common/CartSummary/CartSummary.js
@@ -1,26 +1,20 @@
-const CartSummary = ({ id, name }) => {
-    const cartItems = useSelector((state) => state.cart.cartItems);
+import { useSelector } from 'react-redux';
+import styles from './CartSummary.module.scss';
+import CartItem from '../CartItem/CartItem';
 
-    return (
-        <div className={styles.root}>
-            <h2>Cart Summary</h2>
-            {cartItems.map((item) => (
-                    <div className={styles.image}>
-                    <img src={image} alt={name} />
-                  </div>
-                  <div className={styles.details}>
-                    <h3>{name}</h3>
-                    <p>Quantity: {quantity}</p>
-                    <p>Price: {price}</p>
-                  </div>
-                  <div className={styles.notes}>
-                    <label htmlFor="notes">Notes</label>
-                    <textarea
-                      name="notes"
-                      onChange={(e) => setNotes(e.target.value)}
-                      value={notes}
-                    />
-                  </div>}
+const CartSummary = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+
+  return (
+    <div className={styles.root}>
+      <h2>Cart Summary</h2>
+      {cartItems.map((item) => (
+        <div key={item.productId}>
+          <CartItem {...item} summary={true} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CartSummary;
