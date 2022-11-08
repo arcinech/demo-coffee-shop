c93e7282e64c6728457ab064dc428598bbf70fc0
Marcin Lech <arcinech@gmail.com>
2022-11-08 23:43:20 +0100

    Fix: frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import 'styles/global.scss';
+import './styles/global.scss';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/views/Home/Home';
-import SingleProduct from './components/views/SingleProduct/SingleProduct';
-import Cart from './components/views/Cart/Cart';
+import Home from './components/pages/Home/Home';
+import SingleProduct from './components/pages/SingleProduct/SingleProduct';
+import Cart from './components/pages/Cart/Cart';
 import OrderPage from './components/pages/OrderPage/OrderPage';
+import OrderConfirmation from './components/pages/OrderConfirmation/OrderConfirmation';
+import Catalog from './components/pages/Catalog/Catalog';
+import NotFound from './components/pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -13,9 +16,12 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/orderpage" element={<OrderPage />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/order-confirmation" element={<OrderConfirmation />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
