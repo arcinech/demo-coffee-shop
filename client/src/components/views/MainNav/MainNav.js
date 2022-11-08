1fd9f703d480f2d3fce1511ab18061e37e3dc0d8
Marcin Lech <arcinech@gmail.com>
2022-11-08 16:40:18 +0100

    Fix: header and footer

--- client/src/components/views/MainNav/MainNav.js
+++ client/src/components/views/MainNav/MainNav.js
@@ -1,14 +1,33 @@
 import styles from './MainNav.module.scss';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMugHot } from '@fortawesome/free-solid-svg-icons';
 
 const MainNav = () => {
   return (
     <nav className={styles.root}>
       <div className={styles.container}>
         <div className={styles.left}>
-          <p>Logo</p>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            <FontAwesomeIcon icon={faMugHot} />
+          </NavLink>
         </div>
         <div className={styles.right}>
-          <p>Menu</p>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            Cart
+          </NavLink>
         </div>
       </div>
     </nav>
