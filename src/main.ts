f791ee879fa4296b3da5d6950178f55576dfa102
Marcin Lech <arcinech@gmail.com>
2022-11-09 00:21:32 +0100

    Remove:  user auth module - unfinished

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as session from 'express-session';
-import * as passport from 'passport';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.use(
-    session({
-      secret: 'secret',
-      resave: false,
-      saveUninitialized: false,
-      cookie: { maxAge: 3600000 },
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
+
   app.useStaticAssets(join(__dirname, '..', 'assets', 'public'), {
     prefix: '/assets/',
   });
