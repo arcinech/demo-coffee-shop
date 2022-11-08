6e28db9c1061d033bf60781579d5b7a5330337cb
Marcin Lech <arcinech@gmail.com>
2022-11-08 23:41:40 +0100

    Fix: single product page

--- client/src/components/pages/SingleProduct/SingleProduct.js
+++ client/src/components/pages/SingleProduct/SingleProduct.js
@@ -1,17 +1,18 @@
 import styles from './SingleProduct.module.scss';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import ProductBox from '../../common/ProductBox/ProductBox';
+import { findProduct } from '../../../redux/productSlice';
 
 const SingleProduct = () => {
   const { id } = useParams();
-  const product = useSelector(({ products }) =>
-    products.find((product) => product.id === id),
-  );
+  const product = useSelector((state) => findProduct(state, id));
+  console.log(product);
 
   return (
     <section className={styles.root}>
       <div className={styles.container}>
-        <h1>Single Product</h1>
+        <ProductBox {...product} grid={false} />
       </div>
     </section>
   );
